668df471be9c15023be8a805a9e2909cb08d3387
JacksieCheung <1799902904@qq.com>
2020-12-01 20:11:12 +0800

    usual practice

--- oop/indexOperator/indexOperator.cpp
+++ oop/indexOperator/indexOperator.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cstring>
 
 using namespace std;
 
@@ -13,7 +14,34 @@ class String{
 
         String(const char* p = NULL){
             if (p){
-                delete []Str;
+                len = strlen(p);
+                Str = new char[len+1];
+                strcpy(Str,p);
+            } else {
+                len = 0;
+                Str = NULL;
             }
         }
+
+        ~String(){
+            if (Str!=NULL) delete[] Str;
+        }
+
+        char &operator[](int n){ // 不重载要访问 str 成员
+            return *(Str+n);
+        }
+
+        const char &operator[](int n)const{ // 处理 const 对象
+            return *(Str+n);
+        }
 };
+
+int main(){
+    String S1("0123456789zbcdef");
+    S1.ShowStr();
+    S1[10] = 'A';
+    S1.ShowStr();
+    const String S2("ABCDEFGH");
+    cout<<S2[10]<<endl;
+    return 0;
+}
